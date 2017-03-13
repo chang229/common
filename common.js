d36280ee9875d9f34409ec37fda91ac73f36b9ff
chang229 <963515608@qq.com>
2017-03-13 14:05:20 +0800

    add AJAX

--- common.js
+++ common.js
@@ -316,3 +316,65 @@ function xhr(){
 	};
 	return xhr;
 };
+
+/**
+ * 封装 AJAX方法函数
+ * @param element
+ * @returns {*}
+ */
+function ajax(obj){
+	// 设置默认对象
+	var defares = {
+		type:"get",
+		url:"#",
+		data:{},
+		asynce:true,
+		dataType:"json",
+		success:function(data){console.log(data)}
+	};
+	// 根据传入的参数修改默认参数
+	for( k in obj ){
+		defares[k] = obj[k];
+	};
+	// 创建xhr对象
+	var xhr = null;
+	if( window.XMLHttpRequest ){
+		xhr = new XMLHttpRequest();
+	}else{
+		xhr = new ActiveXObject("Msxml2.XMLHTTP");
+	};
+	// 准备发送
+	var parma = "";
+	for( attr in defares.data ){
+		parma += attr + "=" + defares.data[attr] + "&";
+	};
+	parma = encodeURI( parma.substring(0,parma.length -1 ) );
+	if( defares.type == "get" ){
+		defares.url += "?" + parma;
+	};
+	xhr.open(defares.type,defares.url,defares.asynce);
+	// 发送请求
+	var data = null;
+	if( defares.type == "post" ){
+		data = parma;
+		xhr.setRequestHeader("Content-Type","Application/x-www-form-urlencoded");
+	};
+	xhr.send( data );
+	// 同步请求处理
+	if( !defares.asynce ){
+		if( defares.dataType == "json" ){
+			return JSON.parse( xhr.responseText );
+		};
+		return xhr.responseText;
+	};
+	// 异步请求处理服务器返回的数据
+	xhr.onreadystatechange = function(){
+		if( xhr.readyState == 4 && xhr.status == 200 ){
+			var data = xhr.responseText;
+			if( defares.dataType == "json" ){
+				data = JSON.parse( data );
+			};
+			defares.success(data);
+		};
+	};
+};
